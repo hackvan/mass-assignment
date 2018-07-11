2876ce5f9f35b1bd647533c92254d6936ad848c5
Diego Camacho <hackvan@gmail.com>
2018-07-11 14:27:22 -0500

    Comments to explain send command.

--- lib/person.rb
+++ lib/person.rb
@@ -6,10 +6,13 @@ class Person
   def initialize(attributes=nil)
     # Use only the attribute keys passed into Person.new
     # Attributes are optional
-    
     if attributes
       attributes.each do |key, value|
-        self.send( ("#{key}="), value )
+        # send calls the method in the first parameter, and passes 
+        # the rest of the parameters as arguments.
+        self.send("#{key}=", value)
+        # self.send "name=", "John Smith"
+        # self.name = "John Smith"
       end
     end
   end
